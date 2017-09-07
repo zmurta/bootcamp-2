21a11e1590577e06e1bc4d103ba92bf36246fe6f
Zach <zmurta@ufl.edu>
2017-09-06 23:11:55 -0400

    Initial Finished

--- listingController.js
+++ listingController.js
@@ -7,58 +7,25 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.listings.forEach( (value, index) => {
-      $scope.listings[index].searched = true
-    });
-
-    //Search bar functionality added
-    $scope.search = () => {
-      let searchInput = $scope.searchInput.toUpperCase();
-
-      $scope.listings.forEach( (value, index) => {
-        if(value.code.toUpperCase().indexOf(searchInput) != -1 ||
-            value.name.toUpperCase().indexOf(searchInput) != -1) {
-          $scope.listings[index].searched = true
-        }
-        else $scope.listings[index].searched = false;
-      });
-    }
-
     $scope.addListing = function() {
-      let code = $scope.addCode;
-      
-      $scope.listings.push({
-        "code" : $scope.addCode,
-        "name" : $scope.addBuilding,
-        "address" : $scope.addAddress,
-        "searched" : true
-      });
-      
-      alert(code + " was added to the bottom of your list!");
-            
-      $scope.addCode = '';
-      $scope.addBuilding = '';
-      $scope.addAddress = '';
+      var newListing = {
+        "code": this.code.toUpperCase(),
+        "name": this.name,
+        "coordinates": {
+          "latitude": this.latitude,
+          "longitude": this.longitude
+        },
+        "address": this.address
+      }
+      $scope.listings.push(newListing);
     };
 
     $scope.deleteListing = function(index) {
-      delete $scope.listings[index];
+      $scope.listings.splice(index,1);
     };
 
     $scope.showDetails = function(index) {
-      $scope.currentBuilding = $scope.listings[index].name;
-      
-        if(!$scope.listings[index].address) $scope.currentAddress = "None Available";
-        else $scope.currentAddress = $scope.listings[index].address;
-      
-        if(!$scope.listings[index].coordinates) {
-          $scope.currentLatitude = "None Available";
-          $scope.currentLongitude = "None Available";
-        }
-        else {
-          $scope.currentLatitude = $scope.listings[index].coordinates.latitude;
-          $scope.currentLongitude = $scope.listings[index].coordinates.longitude;
-        }
+      $scope.detailedInfo = $scope.listings[index];
     };
   }
 ]);
